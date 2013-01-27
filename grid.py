77c330ba8e77a19b6bcff514429531493129d6ed
Fenda Truong <sanspei@gmail.com>
2013-01-26 23:15:37 -0600

    first commit

---
+++ grid.py
@@ -0,0 +1,33 @@
+##########################
+#
+# I did it wrong...
+# Location should refer to grid
+#
+#########################
+grid = []
+fp = open("world.txt")
+row = fp.readline()[:-1]
+while(len(row) != 0):
+    col = list(row)
+    grid.append(col)
+    row = fp.readline()[:-1]
+fp.close()
+
+class Grid:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        gridString = ""
+        global grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                gridString += grid[i][j] 
+            gridString+= "\n"
+        return gridString
+
+    def isValid(self, block):
+        if(block == "o"):
+            return False
+        else: 
+            return True
